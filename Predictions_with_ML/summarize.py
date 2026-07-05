cb61a77690e91fab376a91cf68d7433b1874fe61
schmidth-hal <thomas.schmid@medizin.uni-halle.de>
2026-07-06 01:46:50 +0200

    Update summarize.py

--- Predictions_with_ML/summarize.py
+++ Predictions_with_ML/summarize.py
@@ -1,3 +1,19 @@
+#
+# ██████╗  ██████╗ ███████╗███████╗     ██╗
+# ██╔══██╗██╔═══██╗██╔════╝██╔════╝     ██║
+# ██║  ██║██║   ██║███████╗█████╗       ██║
+# ██║  ██║██║   ██║╚════██║██╔══╝       ██║
+# ██████╔╝╚██████╔╝███████║███████╗     ██║
+# ╚═════╝  ╚═════╝ ╚══════╝╚══════╝     ╚═╝
+#
+# A Project by
+# SAFE-AI Consortium | UNIVERSITÄTSMEDIZIN HALLE
+# https://safe-ai-research.github.io
+#
+# Code Author: Quang Vu Nguyen
+# Licence: GNU GPLv3
+#
+
 from pathlib import Path
 import numpy as np
 import re
